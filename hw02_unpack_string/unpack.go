1b1acf4134f6134975001eeb586e22e8e3402a75
root <root@DESKTOP-6MRN4OB.localdomain>
2022-06-10 22:07:50 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,35 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(str string) (string, error) {
 	// Place your code here.
-	return "", nil
+	runes := []rune(str)
+	var sb strings.Builder
+	escaping := false
+
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == '\\' && !escaping {
+			escaping = true
+			continue
+		}
+		if unicode.IsDigit(runes[i]) && !escaping {
+			if i == 0 || (i < len(runes)-1 && unicode.IsDigit(runes[i+1])) {
+				return "", ErrInvalidString
+			}
+			if num, err := strconv.Atoi(string(runes[i])); err == nil {
+				sb.WriteString(strings.Repeat(string(runes[i-1]), num))
+			}
+		} else if i == len(runes)-1 || i < len(runes)-1 && !unicode.IsDigit(runes[i+1]) {
+			sb.WriteRune(runes[i])
+		}
+		escaping = false
+	}
+	return sb.String(), nil
 }
